d1443e1468a230f7498242c6064f127a38320eaa
u-masao <4973920+u-masao@users.noreply.github.com>
2021-11-07 12:00:48 +0900

    add Board class. add data load.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,41 @@
 import argparse
 
+import numpy as np
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="ナンバープレイスを解きます。")
 
     parser.add_argument(
         "infile",
-        nargs=1,
-        type=argparse.FileType("r"),
     )
     return parser.parse_args()
 
 
+class Board:
+    def __init__(self, infile=None, size=9):
+        self.data = np.zeros((size, size)).astype(int)
+
+        if infile is not None:
+            self.load(infile)
+
+    def load(self, filename):
+        question = np.loadtxt(filename, delimiter=",", dtype=str)
+        question[question == " "] = 0
+        question = question.astype(int)
+        self.data = question
+
+    def __str__(self):
+        return "board: \n" + str(self.data).replace("0", ".")
+
+
 def main():
     args = get_args()
-    print(vars(args))
+    print("args:\n", vars(args))
+    board = Board()
+    print(board)
+    board = Board(infile=args.infile)
+    print(board)
 
 
 if __name__ == "__main__":
