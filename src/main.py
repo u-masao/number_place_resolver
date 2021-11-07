5fd2ebd5e7eca89f4ccdc7f9f6ce4335079c64d1
u-masao <4973920+u-masao@users.noreply.github.com>
2021-11-07 11:27:38 +0900

    print args

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 import argparse
 
-def get_args():
-    parse = argparse.ArgumentParse(
-        description='ナンバープレイスを解きます。')
 
-    parse.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+def get_args():
+    parser = argparse.ArgumentParser(description="ナンバープレイスを解きます。")
+
+    parser.add_argument(
+        "infile",
+        nargs=1,
+        type=argparse.FileType("r"),
+    )
     return parser.parse_args()
+
 
 def main():
     args = get_args()
-    print(args)
+    print(vars(args))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
